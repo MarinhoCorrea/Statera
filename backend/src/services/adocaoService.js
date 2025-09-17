1868884794d507ba7922d2442a44f1d583d0708f
PEDROisaias <pedrogomes123md@gmail.com>
2025-09-17 12:01:35 -0300

    criacao da rota adocao e adicao do Swagger

--- backend/src/services/adocaoService.js
+++ backend/src/services/adocaoService.js
@@ -1,54 +1,55 @@
-import { PedidoAdocao } from '../models/PedidoAdocao';
-import { Tutor } from '../models/Tutor.js';
-import { Animal } from '../models/Animal.js';
+    import { PedidoAdocao } from '../models/PedidoAdocao';
+    import { Tutor } from '../models/Tutor.js';
+    import { Animal } from '../models/Animal.js';
 
-export const CreateAdocaoService = async ({ tutor_id, animal_id }) => {
-    try {
-        // Verifica a existencia do animal e do tutor
-        const [tutor, animal] = await Promise.all([
-            Tutor.findbyPk(tutor_id),
-            Animal.findbyPk(animal_id),
-        ]);
-        
-        if (!tutor | !animal) {
-            throw new AppError('Tutor ou animal não encontrado', 404);
+    export const CriarAdocaoService = async ({ tutor_id, animal_id }) => {
+        try {
+            // Verifica a existencia do animal e do tutor
+            const [tutor, animal] = await Promise.all([
+                Tutor.findbyPk(tutor_id),
+                Animal.findbyPk(animal_id),
+            ]);
+            
+            if (!tutor | !animal) {
+                console.error('Tutor ou animal não encontrado');
+            }
+            // Verifica se o tutor respondeu o questionario
+            if (!tutor.questionario_preenchido) {
+                console.error('O tutor ainda não respondeu o questionário obrigatório'
+                );
+            }
+
+            // Verifica a existência de um pedido de adocao deste tutor ou animal
+            const pedidoExistente = await PedidoAdocao.findOne({
+                tutor_id,
+                animal_id,
+                status: 'em analise',
+            });
+
+            if (pedidoExistente) {
+                console.error('Este tutor já tem um pedido de adoção para este animal');
+            } 
+
+            // Cálculo da posição na fila
+            const totalPedidosParaAnimal = await PedidoAdocao.count({
+                where: { animal_id },
+            });
+
+            // A posição do novo pedido será o total + 1
+            const novaPosicao = totalPedidosParaAnimal + 1;
+
+            // Criação do novo pedido
+            const novoPedido = await PedidoAdocao.create ({
+                tutor_id,
+                animal_id,
+                status: 'em analise',
+                posicao_fila: novaPosicao,
+            });
+
+            return novoPedido;
+
+        } catch (error) {
+            console.error('Erro no CreateAdocaoService', error.message);
+            console.error('Erro ao registrar o pedido de adoção');
         }
-        // Verifica se o tutor respondeu o questionario
-        if (!tutor.questionario_preenchido) {
-            throw new AppError('O tutor ainda não respondeu o questionário obrigatório', 400);
-        }
-
-        // Verifica a existência de um pedido de adocao deste tutor ou animal
-        const pedidoExistente = await PedidoAdocao.findOne({
-            tutor_id,
-            animal_id,
-            status: 'em analise',
-        });
-
-        if (pedidoExistente) {
-            throw new AppError('Este tutor já tem um pedido de adoção para este animal', 409);
-        } 
-
-        // Cálculo da posição na fila
-        const totalPedidosParaAnimal = await PedidoAdocao.count({
-            where: { animal_id },
-        });
-
-        // A posição do novo pedido será o total + 1
-        const novaPosicao = totalPedidosParaAnimal + 1;
-
-        // Criação do novo pedido
-        const novoPedido = await PedidoAdocao.create ({
-            tutor_id,
-            animal_id,
-            status: 'em analise',
-            posicao_fila: novaPosicao,
-        });
-
-        return novoPedido;
-
-    } catch (error) {
-        console.error('Erro no CreateAdocaoService', error.message);
-        throw new AppError('Erro ao registrar o pedido de adoção', 500);
-    }
-};
+    };
