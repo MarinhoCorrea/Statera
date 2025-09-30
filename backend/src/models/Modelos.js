ad38112bbbd8091c1010f0a284c202d9c118fbb1
ThiagoLMattos <thiago.mattos3@etec.sp.gov.br>
2025-09-30 05:08:05 -0300

    Update Modelos.js
    ! Modificação em comentários, necessário permissão de modificar

--- backend/src/models/Modelos.js
+++ backend/src/models/Modelos.js
@@ -16,13 +16,36 @@ export const Questionario = QuestionarioModel(sequelize);
 export const PedidoAdocao = PedidoAdocaoModel(sequelize);
 export const Doacao = DoacaoModel(sequelize);
 
+/*
+
 // Associações
 // Explicação das associações:
 // - Um Tutor tem um Questionario.
+
+// Necessário confirmar se será permitido modificar esse arquivo para continuar.
+// Caso possa, modificar certas coisas nos outros modelos, como validações básicas de 'isEmail' etc, e modificar neste:
+
+
+// Tutor.hasOne(Questionario, { foreignKey: 'tutorId' });
+// Questionario.belongsTo(Tutor, { foreignKey: 'tutorId' });
+
 // - Um Tutor pode ter vários Pedidos de Adoção.
+
+// Tutor.hasMany(PedidoAdocao, { foreignKey: 'tutorId' });
+// PedidoAdocao.belongsTo(Tutor, { foreignKey: 'tutorId' });
+
 // - Um Animal pode ter vários Pedidos de Adoção.
+
+// Animal.hasMany(PedidoAdocao, { foreignKey: 'animalId' });
+// PedidoAdocao.belongsTo(Animal, { foreignKey: 'animalId' });
+
 // A tabela PedidosAdocao serve como uma tabela de junção entre Tutores e Animais.
 
+// O modelo Doacao pode estar relacionado a um Tutor, se for o caso:
+// Tutor.hasMany(Doacao, { foreignKey: 'tutorId' });
+// Doacao.belongsTo(Tutor, { foreignKey: 'tutorId' });
+
+*/
 await sequelize.sync();
 
 export default { sequelize, Animal, Tutor, Questionario, PedidoAdocao, Doacao };
